2d1aea95b134140f91b64c849baca543713bdbf1
kevin <Elmer@DESKTOP-VN1F8HC>
2023-06-18 20:02:47 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio Entry/Ejercicio Entry/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio_Entry
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            String pasado = e.OldTextValue;
+            String nuevo = e.NewTextValue;
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            String text = ((Entry)sender).Text;
+        }
+    }
+}
